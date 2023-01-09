e58efbb08c563d1c97ae3d51d85711feb76703f8
ChandarAwesome <ajaygames00Gmail.com>
2023-01-09 21:54:27 +0530

    Basic Core Programs

---
+++ BasicCorerograms/BasicCorerograms/EvenorOdd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorerograms
+{
+    public class EvenorOdd
+    {
+        public static void Evenodd()
+        {
+            Random random = new Random();
+            int number = random.Next();
+            //
+            if(number%2==0)
+            {
+                Console.WriteLine("It is a even number :"+number);
+            }
+            else
+            {
+                Console.WriteLine("It is a odd number :"+number);
+            }
+        }
+    }
+}
---
+++ BasicCorerograms/BasicCorerograms/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicCorerograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //HeadorTails.Cointoss();
+            //LeapYear.Leap();
+            //EvenorOdd.Evenodd();
+            //SwapNumbers.Swap();
+            //Largestnumber.Larger();
+            //Powerof2.power();
+            //QuotientandReminder.Divison();
+            //VowelandConstant.Alphabet('a');
+            //HarmonicNumber.harmonic();
+            PrimeFactor.Factor();
+        }
+    }
+}
