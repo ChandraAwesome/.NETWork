618510fd46a309efb13b5c95b6921eb1e44689e5
ChandarAwesome <ajaygames00Gmail.com>
2023-01-10 17:53:35 +0530

    Logical Programs

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // FibonacciSeries.Fibonac();
+            //PerfectNumeber.Perfect();
+            //PrimeNumber.Prime();
+            ReverseNumber.Reverse(245);
+        }
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class ReverseNumber
+    {
+        public static void Reverse(int a)
+        {
+            int reverse = 0;
+            Console.WriteLine("Number is :" + a);
+           for(;a!=0;a=a/10)
+            {
+                int remainder = a%10;
+                reverse = reverse * 10 + remainder; 
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
